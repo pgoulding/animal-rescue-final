b05067d20a7856c49e5b11438f5e527bf230b7ee
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-31 11:20:52 -0600

    add id and store/fetch method

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-
+import { addDonation } from './actions'
+import { fetchCalls } from './apiFetch';
 export class Form extends Component {
   constructor(){
     super()
@@ -15,20 +16,34 @@ export class Form extends Component {
   }
 
   handleSubmit = (e) => {
-    e.epreventDefault()
-    
+    e.preventDefault()
+    const newDonation = { 
+      ...this.state, 
+      id: Date.now()
+    }
+    const options ={
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newDonation)
+    }
+    fetchCalls('donations/', options)
+    this.props.addDonation([newDonation])
   } 
 
   render() {
     return (
       <form>
         <input
+          placeholder="Name"
           name="name"
           type="text"
           value={this.state.name}
           onChange={this.handleChange}
         />
         <input
+          placeholder="Donation Amount"
           name="donation"
           type="number"
           value={this.state.donation}
@@ -47,7 +62,7 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  
+  addDonation: (donation) => dispatch(addDonation(donation))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Form)
