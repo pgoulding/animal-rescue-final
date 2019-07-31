fb8f2f6325cded30a168f55bd4f9a343d7a85488
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-31 12:01:59 -0600

    WIP convert to thunks

--- src/apiFetch.js
+++ src/apiFetch.js
@@ -1,7 +1,10 @@
+
+
 export const fetchCalls =async (url, options) => {
   const base = `http://localhost:3001/api/v1/${url}`
   try {
     const response = await fetch(base, options)
+    
     const results = await response.json()
     console.log(options)
     return results
