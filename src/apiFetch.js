9f3f35b41f2447f63998070a62f81ad46b2108b2
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-31 11:23:17 -0600

    refacotor fetch for sending correct donation options

--- src/apiFetch.js
+++ src/apiFetch.js
@@ -1,9 +1,9 @@
-const fetchAnimals =async (url, options) => {
+export const fetchCalls =async (url, options) => {
   const base = `http://localhost:3001/api/v1/${url}`
   try {
-    
     const response = await fetch(base, options)
     const results = await response.json()
+    console.log(options)
     return results
   } catch (error) {
     throw Error(error.message)
