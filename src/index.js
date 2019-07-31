40c89d359db0275cbc3d088a71df18855e6fda7e
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-31 12:01:40 -0600

    WIP convert to Thunks

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension'
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
+import thunk from 'redux-thunk';
 
-const store= createStore(rootReducer, composeWithDevTools())
+const store= createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
