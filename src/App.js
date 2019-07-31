83ef8a17ae40118e5b914faa55897d9c60942dd7
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-31 11:21:41 -0600

    WIP is loading/has errored

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import Form from './Form';
+import {connect} from 'react-redux'
+import Donations from './Donations';
+import Animals from './Animals';
+import { fetchCalls } from './apiFetch';
+import {addDonation, hasErrored, isLoading, addAnimals} from "./actions" 
+class App extends Component {
+  
+  async componentDidMount() {
+    this.props.isLoading(true)
+    const animals = await fetchCalls('rescue-animals')
+    this.props.addAnimals(animals)
+    const donation = await fetchCalls('donations')
+    this.props.addDonation(donation)
+  
+  }
+  
+  render () {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    if(this.isLoading) {
+      return <img src='/images/giphy.gif' alt="loading page, please wait"/>
+    }
 
-export default App;
+    if(this.hasErrored) {
+      return <h1>We're sorry there has been an error, please refresh or come back later</h1>
+    }
+    return (
+      <main>
+        <header>
+          <Form />
+          <Donations />
+        </header>
+        <Animals />
+      </main>
+    )
+  }
+
+} 
+
+
+const mapStateToProps = ({hasErrored, isLoading}) => ({
+  hasErrored,
+  isLoading
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  addDonation: (donation) => dispatch(addDonation(donation)),
+  addAnimals: (animals) => dispatch(addAnimals(animals)),
+  isLoading:(loading) =>dispatch(isLoading(loading))
+})
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
+
