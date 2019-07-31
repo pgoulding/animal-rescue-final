fbd55228dcddf388fd5756c761ab67633b557fb2
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-31 11:59:14 -0600

    add loading screen

--- src/App.js
+++ src/App.js
@@ -4,26 +4,30 @@ import {connect} from 'react-redux'
 import Donations from './Donations';
 import Animals from './Animals';
 import { fetchCalls } from './apiFetch';
+import loadingImage from './images/giphy.gif'
+import './App.css'
 import {addDonation, hasErrored, isLoading, addAnimals} from "./actions" 
 class App extends Component {
   
   async componentDidMount() {
-    this.props.isLoading(true)
     const animals = await fetchCalls('rescue-animals')
     this.props.addAnimals(animals)
     const donation = await fetchCalls('donations')
     this.props.addDonation(donation)
-  
+    if(this.props.animals.length) {
+      this.props.loadingPage(false)
+    }
+    console.log(animals.response)
   }
   
   render () {
 
-    if(this.isLoading) {
-      return <img src='/images/giphy.gif' alt="loading page, please wait"/>
+    if(this.props.isLoading) {
+      return <img  className="loading-image" src={loadingImage} alt="loading page, please wait"/>
     }
 
-    if(this.hasErrored) {
-      return <h1>We're sorry there has been an error, please refresh or come back later</h1>
+    if(this.props.hasErrored) {
+      return <h1 className="error-page">We're sorry there has been an error, please refresh or come back later</h1>
     }
     return (
       <main>
@@ -39,15 +43,17 @@ class App extends Component {
 } 
 
 
-const mapStateToProps = ({hasErrored, isLoading}) => ({
+const mapStateToProps = ({hasErrored, isLoading, animals, donations}) => ({
   hasErrored,
-  isLoading
+  isLoading,
+  animals,
+  donations
 })
 
 const mapDispatchToProps = (dispatch) => ({
   addDonation: (donation) => dispatch(addDonation(donation)),
   addAnimals: (animals) => dispatch(addAnimals(animals)),
-  isLoading:(loading) =>dispatch(isLoading(loading))
+  loadingPage:(loading) =>dispatch(isLoading(loading))
 })
 
 
